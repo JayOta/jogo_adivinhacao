8e138de011c9aa440e0638a97326b03090d9ef25
JayOta <otavio.cvilela@gmail.com>
2025-01-17 02:23:38 -0300

    Enjoy <3

---
+++ jogo/Jogo.java
@@ -0,0 +1,79 @@
+package jogo;
+
+import java.util.Random;
+
+public class Jogo {
+    private String[] lista_palavras;
+    private int tamanhoLista;
+    private Random random = new Random();
+    private int num_aleatorio;
+    private String palavra_aleatoria;
+    private String palavraCorreta;
+    private int tamanhoPalavraCorreta;
+    private StringBuilder palavraOculta;
+
+
+
+    public void setLista_palavras(String lista_palavras0, String lista_palavras1, String lista_palavras2, String lista_palavras3, String lista_palavras4, String lista_palavras5, String lista_palavras6, String lista_palavras7, String lista_palavras8, String lista_palavras9) {
+        this.lista_palavras = new String[] {lista_palavras0, lista_palavras1, lista_palavras2, lista_palavras3, lista_palavras4, lista_palavras5, lista_palavras6, lista_palavras7, lista_palavras8, lista_palavras9};
+        setTamanhoLista(this.lista_palavras);
+    }
+    public String[] getLista_palavras() {
+        return this.lista_palavras;
+    }
+
+
+    public void setNum_aleatorio(int tamanhoLista) {
+        if (this.tamanhoLista > 0) {
+        this.num_aleatorio = random.nextInt(tamanhoLista);
+    } else {
+        throw new IllegalArgumentException("A lista de palavras deve conter pelo menos um elemento.");
+    }
+    }
+    public int getNum_aleatorio() {
+        return num_aleatorio;
+    }
+
+
+    public void setTamanhoLista(String[] lista) {
+        this.tamanhoLista = lista.length;
+    }
+    public int getTamanhoLista() {
+        return this.tamanhoLista;
+    }
+
+
+    public void setPalavra_aleatoria(String[] lista) { // <- aqui
+        this.palavra_aleatoria = lista[getNum_aleatorio()];
+        setPalavraCorreta(this.palavra_aleatoria);
+    }
+    public String getPalavra_aleatoria() {
+        return palavra_aleatoria;
+    }
+
+
+    public void setPalavraCorreta(String palavraCorreta) {
+        this.palavraCorreta = palavraCorreta;
+        setTamanhoPalavraCorreta(this.palavraCorreta.length());
+        setPalavraOculta(getPalavraCorreta().length());
+    }
+    public String getPalavraCorreta() {
+        return palavraCorreta;
+    }
+
+
+    public void setTamanhoPalavraCorreta(int tamanhoPalavraCorreta) {
+        this.tamanhoPalavraCorreta = tamanhoPalavraCorreta;
+    }
+    public int getTamanhoPalavraCorreta() {
+        return tamanhoPalavraCorreta;
+    }
+
+
+    public void setPalavraOculta(int tamanhoPalavraCorreta) {
+        this.palavraOculta = new StringBuilder("_".repeat(tamanhoPalavraCorreta));
+    }
+    public StringBuilder getPalavraOculta() {
+        return palavraOculta;
+    }
+}
---
+++ jogo/Main.java
@@ -0,0 +1,64 @@
+package jogo;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String perguntaInicial = "";
+
+        while (!perguntaInicial.equals("sim") && !perguntaInicial.equals("nao")){
+            System.out.println("Seja bem vindo ao Adivinhador de Palavras!\nO jogo será sobre comidas\nDeseja começar?(sim/nao)");
+            perguntaInicial = scanner.nextLine().toLowerCase();
+            if(!perguntaInicial.equals("sim") && !perguntaInicial.equals("nao")){
+                System.out.println("Resposta inválida! Coloque apenas '(sim|nao)'");
+            }
+        }
+        if(perguntaInicial.equals("sim")){
+            Jogo jogo = new Jogo();
+            jogo.setLista_palavras("lasanha", "taco", "churrasco", "omelete", "salada", "bife", "feijoada", "panqueca", "quiche", "risoto"); 
+            jogo.setNum_aleatorio(jogo.getTamanhoLista());
+            jogo.setPalavra_aleatoria(jogo.getLista_palavras());
+            int tentativas = 0;
+            int tentativaMaxima = 10;
+            boolean palavraAcertada = false;
+
+            System.out.println("Vamos jogar então!!\n");
+            while (tentativas < tentativaMaxima && !palavraAcertada) {
+                System.out.println("Palavra oculta: " + jogo.getPalavraOculta());
+                String jogador = scanner.nextLine();
+
+                if(jogador.length() == 1){
+                    char letraTentada = jogador.charAt(0);
+                    boolean acertou = false;
+                    
+                    for(int i = 0; i < jogo.getPalavraCorreta().length(); i++){
+                        if(jogo.getPalavraCorreta().charAt(i) == letraTentada){
+                            jogo.getPalavraOculta().setCharAt(i, letraTentada);
+                            acertou = true;
+                            tentativas--;
+                        }
+                    } if(!acertou) {
+                        System.out.println("Você errou!");
+                    }
+                    tentativas++; 
+
+                    if(tentativas == tentativaMaxima){
+                        System.out.println("Você perdeu!\nA palavra correta era: " + jogo.getPalavraCorreta());
+                    } else if(jogo.getPalavraOculta().toString().equals(jogo.getPalavraCorreta())){
+                        palavraAcertada = true;
+                        acertou = true;
+                        System.out.println("Você ganhou!\nPalavra Acertada: " + jogo.getPalavraOculta().toString());
+                    }
+                } else {
+                    System.out.println("Insira apenas uma letra!");
+                    tentativas--;
+                }
+            }
+        } else if(perguntaInicial.equals("nao")) {
+        System.out.println("Ok, até mais!");
+        scanner.close();
+    }
+    }
+}
